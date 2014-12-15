e6c6103e17ac1c9917e0b6bb6bd2150a82a6a1c0
Philippe Miossec <pmiossec@gmail.com>
2014-12-15 12:04:26 +0100

    Get A unique id for application revision of Clickonce
    or update a ApplicationRevision value in a csproj file

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClickOnceUniqueId
+{
+	class Program
+	{
+		private static DateTime RefDate = new DateTime(2014, 12, 15);
+		static void Main(string[] args)
+		{
+			var uniqueId = (int)(DateTime.Now - RefDate).TotalSeconds;
+			if(args.Length == 0)
+				Console.Write(uniqueId);
+			else
+				UpdateProjectFile(args.First(), uniqueId);
+		}
+
+
+		private static void UpdateProjectFile(string projectFilePath, int? newValue)
+		{
+			var project = new Microsoft.Build.Evaluation.Project(projectFilePath);
+
+			var property = project.GetProperty("ApplicationRevision");
+			if(newValue.HasValue)
+				property.UnevaluatedValue = newValue.Value.ToString();
+			else
+				property.UnevaluatedValue = (System.Int32.Parse(property.EvaluatedValue) + 1).ToString();
+			project.Save();
+
+		}
+	}
+}
